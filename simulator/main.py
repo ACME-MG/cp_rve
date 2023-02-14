a8cd7813b0f0b3215cca9aba7db83991295dbe47
Janzen Choi <janzenchoi@yahoo.com>
2023-02-14 22:45:23 +1100

    new 500 mesh for simulation

--- simulator/main.py
+++ simulator/main.py
@@ -2,4 +2,4 @@ from modules.api import API
 api = API("test", 0)
 api.define_mesh("mesh.e", "input_orientations.csv")
 api.define_params()
-api.simulate("~/moose/deer/deer-opt", 2)
+api.simulate("~/moose/deer/deer-opt", 24)
