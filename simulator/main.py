9f441ce4a3d4c667e9bcb51293bd173c6ba46721
Janzen <janzenchoi@yahoo.com>
2023-04-26 12:20:07 +1000

    removed czm model

--- simulator/main.py
+++ simulator/main.py
@@ -3,4 +3,4 @@ folder = "500_24_s1"
 api = API(f"{folder}_no_gbc", 0)
 api.define_mesh(f"{folder}/mesh.e", f"{folder}/input_orientations.csv")
 api.define_params()
-api.simulate("~/moose/deer/deer-opt", 1)
+api.simulate("~/moose/deer/deer-opt", 12)
