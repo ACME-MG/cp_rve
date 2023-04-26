a19d567cf8ee88901059f458530fac4f96c16bf7
Janzen <janzenchoi@yahoo.com>
2023-04-26 13:01:55 +1000

    added vs czm model

--- simulator/main.py
+++ simulator/main.py
@@ -1,6 +1,6 @@
 from modules.api import API
 folder = "500_24_s1"
-api = API(f"{folder}", 0)
+api = API(f"{folder}_vs_czm", 0)
 api.define_mesh(f"{folder}/mesh.e", f"{folder}/input_orientations.csv")
 api.define_params()
-api.simulate("~/moose/deer/deer-opt", 1)
+api.simulate("~/moose/deer/deer-opt", 12)
